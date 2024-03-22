2d7da6c04b87c72346e2d0d06698615ff29a9ae1
Faiz Ghanchi <faizghanchi04@gmail.com>
2024-03-22 06:59:53 +0530

    Temporarily changes the roles of company and employee

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_BASE_URL || 'http://marketingcompany.tech';
+const BASE_URL = process.env.API_BASE_URL || 'https://marketingcompany.tech';
 
 export default axios.create(
   {
